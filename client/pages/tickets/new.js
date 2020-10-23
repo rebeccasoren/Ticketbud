f441e7543550d509fd7d5665eb07019d5d3652f3
rebeccasoren <sorenrebeccashruti@gmail.com>
2020-10-23 13:38:14 +0530

    css

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -35,6 +35,10 @@ const NewTicket = () => {
 				<div className='form-group'>
 					<label>Title</label>
 					<input
+					style={{width: '70%',
+					padding: '12px 20px',
+					margin: '8px 0',
+					boxSizing: 'border-box'}}
 						value={title}
 						onChange={(e) => setTitle(e.target.value)}
 						className='form-control'
