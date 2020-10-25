e9974bb621c6c1fc5b35a64f26bc198d5d31dda7
rebeccasoren <sorenrebeccashruti@gmail.com>
2020-10-25 11:17:36 +0530

    css files

--- client/pages/orders/index.js
+++ client/pages/orders/index.js
@@ -4,16 +4,16 @@ const OrderIndex = ({ orders }) => {
 		<center><h1>My Orders</h1></center>
 		{orders.map((order) => {
 		  return (
-			<div class="card text-center" key={order.id} style={{width:'50%', color:'#6A706E', marginBottom:'2%'}}>
+			<center>
+				<div class="card text-center" key={order.id} style={{width:'50%', color:'#6A706E', marginBottom:'2%'}}>
 			<div class="card-body">
 				<h5 class="card-title">{order.ticket.title}</h5>
 		  			<p class="card-text">Price: Rs.{order.ticket.price}</p>
 			</div>
 			<div class="card-footer text-muted">{order.status}</div>
 			</div>
-			// <li key={order.id}>
-			//   {order.ticket.title}: {order.status}
-			// </li>
+			</center>
+			
 		  );
 		})}
 	  </div>
